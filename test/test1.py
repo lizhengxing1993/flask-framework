1435874bf5fe0762d590ee431f2771ebae5467ec
L-lizhengxing <L-lizhengxing@qianxin.com>
2020-09-14 16:48:41 +0800

    增加测试

--- test/test1.py
+++ test/test1.py
@@ -3,8 +3,12 @@ import os
 PROJECT_APP_DIR = os.path.dirname(os.path.realpath(__file__))
 PROJECT_DIR = os.path.realpath(os.path.join(PROJECT_APP_DIR, ".."))
 LOG_PATH = os.path.join(PROJECT_DIR, "logs")
+STATIC_PATH = os.path.join(PROJECT_DIR, "static/")
 
 print(PROJECT_APP_DIR)
 print(PROJECT_DIR)
 print(LOG_PATH)
+
+photo_path = STATIC_PATH + '1.JPG'
+print(photo_path)
 
